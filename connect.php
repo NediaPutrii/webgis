3e162aaf03b4e63a13219376a43599815867093e
nediaputrii <50162029+NediaPutrii@users.noreply.github.com>
2020-11-19 01:57:46 +0700

    configurasi connection

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
-	$host = "localhost";
-	$user = "postgres";
-	$pass = "12345678";
-	$port = "5433";
-	$dbname = "turis";
+	$host = "ec2-54-166-114-48.compute-1.amazonaws.com";
+	$user = "cfkdkcqzzckgdm";
+	$pass = "b43055ae09ead5ce0ed8f16edbcc0aa428364372d361e4ff024955ecd01e1711";
+	$port = "5432";
+	$dbname = "d1t9rg7qou5d67";
 	$conn = pg_connect("host=".$host." port=".$port." dbname=".$dbname." user=".$user." password=".$pass) or die("Gagal");
 	// header("Access-Control-Allow-Origin: *");
 ?>
