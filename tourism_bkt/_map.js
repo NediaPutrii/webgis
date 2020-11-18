bab75ceffda635f83b1efaeae1e41a41affaad02
nediaputrii <50162029+NediaPutrii@users.noreply.github.com>
2020-11-19 02:41:02 +0700

    ubah server

--- tourism_bkt/_map.js
+++ tourism_bkt/_map.js
@@ -1,5 +1,5 @@
         var map;
-        var server = "https://webgisobjecttourism.herokuapp.com/";
+        var server = "https://webgisobjecttourism.herokuapp.com/tourism_bkt/";
         var cekRadiusStatus = "off"; //RADIUS
         var circles = []; //RADIUS
         var rad; //RADIUS
